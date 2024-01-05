4f4bcff85fd41d807faa25005316810f52530fce
MulyadiK <141252284+DolphinFlip@users.noreply.github.com>
2024-01-05 15:38:59 +0700

    Update ValidationService.java

--- booking-salon-main-mulyadi/src/main/java/com/booking/service/ValidationService.java
+++ booking-salon-main-mulyadi/src/main/java/com/booking/service/ValidationService.java
@@ -3,10 +3,9 @@ package com.booking.service;
 public class ValidationService {
     // Buatlah function sesuai dengan kebutuhan
     public static void validateInput(){
-        System.out.println("Validation placeholder for general input");
+
     }
 
     public static void validateCustomerId(){
-        System.out.println("Validation placeholder for customer ID");
     }
 }
